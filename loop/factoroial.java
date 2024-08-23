19fb7c1673f736a4ef34fbd582a9ed9acf762d31
raju kumar <150627700+rajusharmadata@users.noreply.github.com>
2024-08-23 22:26:09 +0530

    commit
    cd
    ls

---
+++ loop/factoroial.java
@@ -0,0 +1,12 @@
+public class factoroial {
+    public static void main(String[]args){
+        
+        // find factorial
+        int number =5;
+        int fact =1;
+        for(int i=1;i<=number;i++){
+            fact =fact*i;
+        }
+        System.out.println(fact);
+    }
+}
