5965ca7c0ad7aadea4f22643295c092e40f21e92
raju kumar <150627700+rajusharmadata@users.noreply.github.com>
2024-08-23 22:18:06 +0530

    add

---
+++ basics/Untitled.java
@@ -0,0 +1,22 @@
+public class Untitled {
+
+    public static void main(String[]arg){
+        System.out.println("hello");
+
+// primative type 
+        // data type in  java 
+        // int
+        // float
+        // double
+        // bool
+        // short
+        // byte 
+        // char
+        // long
+
+// input 
+// int n = nextInt();
+
+        
+    }
+}
