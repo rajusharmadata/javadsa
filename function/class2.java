34eb6855c11a24f4d909b52b396f264f10c831bb
raju kumar <150627700+rajusharmadata@users.noreply.github.com>
2024-08-23 22:43:11 +0530

    add

--- function/class2.java
+++ function/class2.java
@@ -1,14 +1,13 @@
-public class class2 {
+// public class class2 {
 
-public static int A(int b){
-    int f = 10;
-}
-public static void main(String arg[]){
-    int a = 5;
-    int ans = A(a);;
-    System.out.println(ans);
-}
+// public static int A(int b){
+//     int f = 10;
+// }
+// public static void main(String arg[]){
+//     int a = 5;
+//     System.out.println(ans);
+// }
     
-}
-    /// only  call by valu use in java
+// }
+//     /// only  call by valu use in java
 
