f3747da21c4fa32b5d5160bd24da696844192b23
raju kumar <150627700+rajusharmadata@users.noreply.github.com>
2024-08-23 22:27:02 +0530

    push

---
+++ function/class2.java
@@ -0,0 +1,14 @@
+public class class2 {
+
+public static int A(int b){
+    int f = 10;
+}
+public static void main(String arg[]){
+    int a = 5;
+    int ans = A(a);;
+    System.out.println(ans);
+}
+    
+}
+    /// only  call by valu use in java
+
