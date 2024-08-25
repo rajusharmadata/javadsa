da038b9da12255e274c278a1a0a7ef57aedb02dd
raju kumar <150627700+rajusharmadata@users.noreply.github.com>
2024-08-25 15:20:08 +0530

    math function

--- function/qus3.java
+++ function/qus3.java
@@ -4,14 +4,15 @@ public class qus3 {
 
     public static boolean plaindrom(int n ){
         int remender;
+        int number = n;
         int ans =0;
         while (n>0) {
-        remender = n%10;
+        remender = n%10; 
         ans = ans*10+remender;
         n = n/10;
         
         }
-       if(ans == n){
+       if(ans == number){
         return true;
        }
        else{
