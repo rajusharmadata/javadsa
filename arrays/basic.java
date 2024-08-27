153bc7d404432e1178343d6254cb2f6a4e3b9bf8
raju kumar <150627700+rajusharmadata@users.noreply.github.com>
2024-08-27 20:21:13 +0530

    add

--- arrays/basic.java
+++ arrays/basic.java
@@ -1,20 +1,23 @@
-
-
+import java.util.*;
 public class basic {
     
     // How to declear a arrays
     //dataType arrayName[] = new dataType[size];
     // new is basicaly look a memory location and take a memory give a memory
     public static void main(String [] args){
-        @SuppressWarnings("unused")
-        int marks[]=new int[50];// aloacte a fifity memory
-        @SuppressWarnings("unused")
+        Scanner sc = new Scanner(System.in);
+        int marks[] = new int[45];
+      
         int number[] = {1,2,3,4,5};
         String fruits[] = {"rahul","raju","niraj","shivam" };
         
         System.out.println(fruits[0]);
         System.out.println(fruits[1]);
         System.out.println(fruits[2]);
+
+        System.out.println("Enter marks");
+       marks[0] =  sc.nextInt();
+    //    sc.nextInt();
 
 }
 }
