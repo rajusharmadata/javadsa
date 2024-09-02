3860097b49ed06336765a8621d67eea715c9d752
raju <rajusharmadata123@gmail.com>
2024-09-02 22:16:38 +0530

    add

---
+++ intervielevelQs/buyandscellstocks.java
@@ -0,0 +1,23 @@
+public class buyandscellstocks {
+    public static int buyandssellstocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxprofit =0;
+
+        for(int i = 0; i<prices.length;i++){
+            if(buyPrice < prices[i]){
+                int profit = prices[i]-buyPrice;
+                maxprofit = Math.max(maxprofit, profit);
+
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxprofit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {6,1,3,5,6,7};
+      System.out.println( buyandssellstocks(prices)); 
+        
+    }
+}
