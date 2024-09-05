56b348cda3d68d489e3709ca96e7d18aa2fad7b4
raju <rajusharmadata123@gmail.com>
2024-09-05 22:52:45 +0530

    add

--- storingAlogrithms/selectionsort.java
+++ storingAlogrithms/selectionsort.java
@@ -1,11 +1,12 @@
 public class selectionsort {
-    public static void selectionsort(int arr[]){
+    public static void selectionSort(int arr[]){
+
          for(int i = 0;i<arr.length-1 ; i++){
             int minpos = i;
-            for(int j = i+1;j<arr.length-1;j++){
-                if(arr[minpos]>arr[i]){
-                    minpos = j;
-                }
+          for(int j = i+1;j<arr.length;j++){
+            if(arr[minpos]>arr[j]){
+                minpos = j;
+            }
            
           }
           // Swap
@@ -17,7 +18,7 @@ public class selectionsort {
     }
     public static void main (String[]args){
     int arr[] = {5,4,1,3,2};
-    selectionsort(arr);
+    selectionSort(arr);
     for(int i = 0;i<arr.length;i++){
         System.out.print(arr[i]+" ");
     }
