c25ffb6cd9fd5c867167378721343255185a9a33
raju <rajusharmadata123@gmail.com>
2024-09-07 22:54:15 +0530

    add

--- storingAlogrithms/contingsort.java
+++ storingAlogrithms/contingsort.java
@@ -9,20 +9,22 @@ public class contingsort {
         for (int i = 0; i < arr.length; i++) {
             count[arr[i]]++;
         }
-        
-        int k = 0;
-        for (int i = 0; i <= max; i++) {
-            for (int j = 0; j < count[i]; j++) {
-                arr[k++] = i;
+
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
             }
         }
     }
     public static void main(String[] args) {
-        int[] arr = {1,4,1,3,2,4,3,7};
+        int[] arr = {1,4,1,3,2,4,3,7 };
         countingSort(arr);
         for(int i = 0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
     }
-    
-}
+    }
+
