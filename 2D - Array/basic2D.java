e4342a3cc15e590acede8fb14415440453b1b274
raju <rajusharmadata123@gmail.com>
2024-09-11 23:35:57 +0530

    add new file

--- 2D - Array/basic2D.java
+++ 2D - Array/basic2D.java
@@ -5,13 +5,14 @@ public class basic2D {
         Scanner sc = new Scanner(System.in);
         int matrix[][] = new int [3][3];
         int n = matrix.length;
-        int m = matrix.length;
+        int m = matrix[0].length;
         // input of 2D array
         for(int i = 0;i<n;i++){// Denoted the row
             for(int j = 0;j<m;j++){// Denoted the column
               matrix[i][j] = sc.nextInt();
             }
         }
+        System.out.println();
         // output 
         for(int i = 0;i<n;i++){// Denoted the row
             for(int j = 0;j<m;j++){// Denoted the column
---
+++ 2D - Array/question1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class question1 {
+    public static boolean Search(int matrix[][],int key){
+        for(int i = 0;i<matrix.length;i++){
+            for(int j = 0;j<matrix.length;j++){
+                if(matrix[i][j]==key){
+                    System.out.println("index is (" + i + " " + j + " )");
+                    return true;
+                }
+            }
+        }
+        System.out.println("index is not found ");
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int matrix[][] = new int [3][3];
+        int m = matrix.length,n = matrix[0].length;
+        // input 
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        int key = 7;
+        Search(matrix, key);
+    }
+}
