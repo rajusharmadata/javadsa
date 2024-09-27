03bebcd623a9e45a2d950bfd9ff324a09a03a1db
rajusharma <rajusharmadata123@gmail.com>
2024-09-28 04:10:30 +0530

    add value

---
+++ Strings/Strings.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Strings{
+    public static void main(String[] args) {
+        // declartion 
+       char arr[] = {'a','b','c','d'};
+       String str = "abacd";
+       String str1 = new String("xyz");
+       Scanner sc = new Scanner(System.in);
+       String name;
+       // input
+    //    name = sc.next();// sigle word
+            name = sc.nextLine();// multiple words
+        // output 
+        System.out.println(name);
+    }
+}
