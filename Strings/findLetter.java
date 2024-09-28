be15df8d4d70831f08a70463e01f9db3f982f0aa
rajusharma <rajusharmadata123@gmail.com>
2024-09-29 00:57:44 +0530

    add new file

---
+++ Strings/findLetter.java
@@ -0,0 +1,12 @@
+public class findLetter {
+    public static void FindLetter(String str){
+        for(int i = 0;i<str.length();i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        String str = "Raju sharma";
+        FindLetter(str);
+    }
+}
