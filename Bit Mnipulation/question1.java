72e831bae4e2779ba7e3ce73751ad5815c12395d
rajusharma <rajusharmadata123@gmail.com>
2024-10-04 19:55:42 +0530

    add new file

--- Bit Mnipulation/question1.java
+++ Bit Mnipulation/question1.java
@@ -20,7 +20,7 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter number");
     int n = sc.nextInt();
-    Evenodd(n);
+    Evenodd(-6);
    
 }
 }
