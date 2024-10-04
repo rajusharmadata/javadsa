ed396d11cbc0df1562a22802d71009522bb3b7d3
rajusharma <rajusharmadata123@gmail.com>
2024-10-04 20:33:49 +0530

    :addgi9t

--- Bit Mnipulation/question1.java
+++ Bit Mnipulation/question1.java
@@ -20,7 +20,7 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter number");
     int n = sc.nextInt();
-    Evenodd(-6);
-   
+    Evenodd(n);
+   sc.close();
 }
 }
