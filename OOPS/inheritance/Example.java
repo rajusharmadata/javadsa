03bdd8a6ac55eb55c4782c037bffbd46e97fbbb4
raju sharma <rajusharmadata123@gmail.com>
2024-10-15 23:30:59 +0530

    add

--- OOPS/inheritance/Example.java
+++ OOPS/inheritance/Example.java
@@ -1,12 +1,21 @@
-
+// Example of inhertence 
 
 public class Example {
     
     public static void main(String[] args) {
-        fish shark = new fish();
-        shark.eat();
+        // fish shark = new fish();
+        // shark.eat();
+
+        Dog pupe = new Dog();
+        pupe.eat();
+        pupe.breathe();
+        pupe.breads();
     }
 }
+/*
+ * 
+ * 1. Single level inhertance 
+ * 
 // base class
 class Animal {
     String color;
@@ -29,3 +38,34 @@ class fish extends Animal{
      }
     
 }
+
+ */
+
+
+ // 2. multlevel inhertance 
+
+ class Animal {
+    String color;
+
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathes");
+    }
+
+ }
+
+ class mameal extends Animal{
+    int leg;
+    void legs(int leg){
+        this.leg = leg;
+    }
+ }
+
+ class Dog extends mameal{
+    void breads(){
+        System.out.println("Dogs is breads........ ");
+    }
+ }
