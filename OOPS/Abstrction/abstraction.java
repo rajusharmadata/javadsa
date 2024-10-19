c24b4b435d4c6ebcb34c24594ef5a4501f29e08d
raju sharma <rajusharmadata123@gmail.com>
2024-10-19 12:04:39 +0530

    add

--- OOPS/Abstrction/abstraction.java
+++ OOPS/Abstrction/abstraction.java
@@ -8,24 +8,35 @@ public class abstraction {
         cheake c = new cheake();
         c.eat();;
         c.walk();
+        System.out.println(c.color);
     }
 }
 
  abstract class Animal{
+    String color;
+    Animal(){
+        color = "brown";
+    }
     void eat(){
         System.out.println("eats ");
     }
-
+    // Give a idea  in walk 
     abstract void walk();
 }
-
+ // implement of the walk idea 
 class Horese extends Animal{
+    void changecolor(){
+        color = "black brown";
+    }
     void walk(){
         System.out.println("Hore walk 4 leags");
     }
 }
-
+ // implement of the walk idea 
 class cheake extends Animal{
+    void changecolor(){
+        color = "Yellow";
+    }
     void walk(){
         System.out.println("chicken walk  2 legs");
     }
