2bd1e403917b03066a8f9453779bc85e4ab0266a
raju sharma <rajusharmadata123@gmail.com>
2024-10-21 14:31:19 +0530

    add" g;

---
+++ OOPS/practice quection/question4.java
@@ -0,0 +1,31 @@
+/*
+ which of the following is a correct statement (both in same package )
+ 1.  car c = new car();
+ 2.  vechical  v = new vechical();
+ 3. vechical v = new car();
+ 4. car c = vechical();// wrong 
+ */
+
+ public class question4 {
+ 
+    public static void main(String[] args) {
+        car c = new car();
+        vechical v = new vechical();
+        // java ke andar ham child obj ka refernce parent ke refernce me assing kar sakte hai
+        vechical v1 = new car();
+        // car ke proprty ko accece nhi kar payenege kyu ki car ka reference nhi vehical ker ander 
+         v1.run();
+    }
+ }
+
+ class vechical {
+    void run(){
+        System.out.println("run");
+    }
+ }
+
+ class car extends vechical{
+    void car(){
+        System.out.println("car is run  ");
+    }
+ }
