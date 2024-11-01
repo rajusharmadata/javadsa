c135e4b572db78cfe771c95981c290708e4dc184
raju sharma <rajusharmadata123@gmail.com>
2024-11-01 22:01:24 +0530

    add

---
+++ 19. Recursion/question11.java
@@ -0,0 +1,9 @@
+/*
+ Tilling problem 
+ Given a "2 x n" board and tiles of size "2 x 1" count the number of ways to tile the given  board 
+ using the 2 x 1tiles.
+ (A tiles can either be placed Horizontally  or vertically )
+ */
+public class question11 {
+    
+}
