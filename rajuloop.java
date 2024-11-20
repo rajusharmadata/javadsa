4abd131e141c976e961f555e3c77d0b3650db32a
raju kumar <rajusharmadata123@gamil.com>
2024-11-20 22:35:08 +0530

    add

---
+++ rajuloop.java
@@ -0,0 +1,19 @@
+public class rajuloop {
+
+    public static void main(String [] args){
+        for(int i = 0;i<7;i++){
+            System.out.print("hello raju " + "   ");
+
+            // object 
+            
+        }
+    }
+}
+
+class raju {
+    int raju = 4;
+       raju(){
+        System.out.println("raju");
+    }
+   
+}
