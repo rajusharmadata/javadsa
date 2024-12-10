0dc73ce3455276c66819664f23e7e9391484f39e
rajusharmadata <rajusharmadata123@gmail.com>
2024-12-10 15:51:57 +0530

    add

--- 18. OOPS/poliymarphisam/Methodoverriding.java
+++ 18. OOPS/poliymarphisam/Methodoverriding.java
@@ -1,22 +1,33 @@
+// This is the main class where we demonstrate method overriding in action
 public class Methodoverriding {
-    public static void main(String [] args){
+    public static void main(String[] args) {
+        // Creating an instance of the subclass `deer`
         deer d1 = new deer();
-        d1.eat();
+        // Calling the `eat()` method on the `deer` object
+        // This will execute the overridden `eat()` method in the `deer` class
+        d1.eat(); // Output: eat grass
+
+        // Creating an instance of the superclass `Animal`
         Animal d2 = new Animal();
-        d2.eat();
+        // Calling the `eat()` method on the `Animal` object
+        // This will execute the `eat()` method in the `Animal` class
+        d2.eat(); // Output: eat any thing
     }
 }
 
+// Base class `Animal` which contains a general `eat()` method
 class Animal {
-    void eat(){
+    // Method to describe eating behavior of an animal
+    void eat() {
         System.out.println("eat any thing");
     }
-   
 }
-class deer extends Animal{
-    void eat(){
+
+// Subclass `deer` that extends the `Animal` class
+class deer extends Animal {
+    // Overriding the `eat()` method to specify that deer eat grass
+    @Override
+    void eat() {
         System.out.println("eat grass");
     }
 }
-
-
